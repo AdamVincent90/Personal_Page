96acfeeecf2c8aca9dff4ffd6ef387cadf11d11d
juddu <adam.vincent16@my.northampton.ac.uk>
2020-08-30 12:53:46 +0100

    remove timestamp

--- app/Grade.php
+++ app/Grade.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Grade extends Model
 {
     protected $fillable = ['id', 'module', 'grade', 'learning_outcome', 'year'];
+    public $timestamps = false;
 }
